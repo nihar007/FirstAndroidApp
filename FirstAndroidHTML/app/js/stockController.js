3e3412eb70c3767d08482f7fb99aeb7df3a0390c
nihar shah <nihar_shah60@yahoo.com>
2013-11-26 19:07:56 -0500

    StockHarcoded

---
+++ FirstAndroidHTML/app/js/stockController.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myController($scope){
+        //I didn't wanted to use window.alert() to show that controller is called first
+          console.log("First: Inside AngularJS Controller: called when DOM tree is created");
+  
+          //Stock price object.
+          $scope.nihar="Nihar";
+          
+          $scope.stockPrice = {
+                "Google":  "600$",
+                "Microsoft": "400$",
+                "VMWare": "500$"
+        };
+
+          //Select VMWare by default
+        $scope.selectedCompany = "VMWare";
+    
+}
